0fdf7fa090a205a3d52e6b2064fc8e1fd04756d1
Toni Andreev <rincerwind@outlook.com>
2015-12-18 21:41:52 +0200

    Modified murmur3 because it seems to not work as expected

--- networkx/algorithms/influence_maximization/murmur3.py
+++ networkx/algorithms/influence_maximization/murmur3.py
@@ -4,6 +4,7 @@ The murmurHash3 function as described by Thomas Pajor
 in https://github.com/Luftschlange/ms-skim/blob/master/src/SKIM.h
 '''
 def murmurHash3(u,v,i,l, randomSeed):
+    randomSeed &= 0xFFFFFFFF
     h = (randomSeed<<16)+l
     
     c1 = 0xcc9e2d51
@@ -11,35 +12,39 @@ def murmurHash3(u,v,i,l, randomSeed):
 
     # Hash the first vertex
     k = u
-    k = (c1 * k) & 0xFFFFFFFF
-    k = ( k << 15 | k >> 17 ) & 0xFFFFFFFF # inline rotl
-    k = ( c2 * k ) & 0xFFFFFFFF
+    k = (c1 * k)
+    k = rol(k, 15, 32)
+    k = ( c2 * k )
     h ^= k
-    h = ( h << 13 | h >> 19 ) & 0xFFFFFFFF # inline rotl
-    h = ( h * 5 + 0xe6546b64 ) & 0xFFFFFFFF
+    h = rol(h, 13, 32)
+    h = ( h * 5 + 0xe6546b64 )
 
     # Hash the second vertex
     k = v
-    k = (c1 * k) & 0xFFFFFFFF
-    k = ( k << 15 | k >> 17 ) & 0xFFFFFFFF # inline rotl
-    k = ( c2 * k ) & 0xFFFFFFFF
+    k = (c1 * k)
+    k = rol(k, 15, 32)
+    k = ( c2 * k )
     h ^= k
-    h = ( h << 13 | h >> 19 ) & 0xFFFFFFFF # inline rotl
-    h = ( h * 5 + 0xe6546b64 ) & 0xFFFFFFFF
+    h = rol(h, 13, 32)
+    h = ( h * 5 + 0xe6546b64 )
 
     # Hash the instance
     k = i
-    k = (c1 * k) & 0xFFFFFFFF
-    k = ( k << 15 | k >> 17 ) & 0xFFFFFFFF # inline rotl
-    k = ( c2 * k ) & 0xFFFFFFFF
+    k = (c1 * k)
+    k = rol(k, 15, 32)
+    k = ( c2 * k )
     h ^= k
 
     # Mix the results
     h ^= 10
     h ^= (h >> 16)
-    h = ( h * 0x85ebca6b ) & 0xFFFFFFFF
+    h = ( h * 0x85ebca6b )
     h ^= (h >> 13)
-    h = ( h * 0xc2b2ae35 ) & 0xFFFFFFFF
+    h = ( h * 0xc2b2ae35 ) 
     h ^= (h >> 16)
     
     return h
+
+rol = lambda val, r_bits, max_bits: \
+    (val << r_bits%max_bits) & (2**max_bits-1) | \
+    ((val & (2**max_bits-1)) >> (max_bits-(r_bits%max_bits)))
--- networkx/algorithms/influence_maximization/tests/test_hc.py
+++ networkx/algorithms/influence_maximization/tests/test_hc.py
@@ -8,8 +8,13 @@ class TestHC:
     def setUp(self):
         # simple graph
         G = nx.DiGraph()
-        G.add_edges_from([(1,2),(3,2),(3,1),(4,2),(3,5),(6,5),(5,6),(7,6),(6,8),(8,6),(7,8),(8,7),(9,6),(10,9),(11,10),(11,12),(13,11)], num_sim = 100, act_prob=1.0)
+        G.add_edges_from([(1,2),(3,2),(3,1),(4,2),(3,5),(6,5),(5,6),(7,6),(6,8),(8,6),(7,8),(8,7),(9,6),(10,9),(11,10),(11,12),(13,11)], num_sim = 1, act_prob=1.0)
         self.G = G
 
     def test_hc(self):
         assert_equal(nx.hill_climbing(self.G, 2), set([13,3]))
+
+    def test_hc(self):
+        G = nx.DiGraph()
+        G.add_edges_from([(1,2),(3,2),(3,1),(4,2),(3,5),(6,5),(5,6),(7,6),(6,8),(8,6),(7,8),(8,7),(9,6),(10,9),(11,10),(11,12),(13,11)], num_sim = 100, act_prob=0.1)
+        assert_equal(nx.hill_climbing(G, 2), set([13,3]))
